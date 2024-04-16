c7833c0eb1424aef1a30bd50134f5dbb35deffbd
Adeilson <adeilsonjunior@gmail.com>
2024-04-15 21:03:47 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dbc_reader
+
+Script R para leitura e análise de arquivos DBC.
+
+1 - Baixar os arquivos conforme parâmetro do usuário (mês inicial, mês final e uf).
+2 - Transformar DBC para TXT.
+3 - Agrupar os dados por gestão, somando valor aprovado.
+4 - Incluir os dados em um dataframe
---
+++ get_data.R
@@ -0,0 +1,37 @@
+##-- Pacotes ----
+## Working Directories
+## Casa
+setwd('C:\\desenvolvimento\\projetos\\Dbc_reader')
+
+library(RCurl)
+library(rvest)
+library(dplyr)
+library(stringr)
+library(read.dbc)
+library(readxl)
+
+source("R/utils.R")
+
+ano <- "2024"
+dir_out <- "outputs"
+log_file <- sprintf("documentos/log_%s.txt", ano)
+unlink(log_file)
+
+##-- SIH ----
+download_sih(ano = ano, dir_out = dir_out, log_file = log_file)
+
+##-- SIA ----
+download_sia(ano = ano, dir_out = dir_out, log_file = log_file)
+
+##-- Informações dos procedimentos ----
+download_dsinfo(level = "forma_organizacao", dir_out = dir_out, log_file = log_file)
+# download_dsinfo(level = "procedimento", dir_out = dir_out, log_file = log_file)
+  
+##-- Informações do CNES ----
+download_cnes(ano = ano, dir_out = dir_out, log_file = log_file)
+
+##-- IBGE ---- FEITO ARTESANALMENTE, usando downloadDatasus.ipynb
+# download_ibge(ano = ano, dir_out = dir_out, log_file = log_file)
+
+##-- ANS ---- FEITO ARTESANALMENTE usando downloadDatasus.ipynb
+# download_ans(ano = ano, mes = "01", dir_out = dir_out, log_file = log_file)
